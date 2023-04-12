7d2d57a95f4136a142c8309e64f7b99bd04f1d49
Liusy Divakov <divakovwork@gmail.com>
2023-04-12 14:07:41 +0300

    Revert "update App.jsx"
    This reverts commit 70580029227ad710cd9cf8225dbc8cc6c11d9933.

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import Sidebar from "./side-bar";
-import Editor from "./editor";
-import { data } from "./data";
-import Split from "react-split";
-import { nanoid } from "nanoid";
+import React from "react"
+import Sidebar from "./side-bar"
+import Editor from "./editor"
+import { data } from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
 
 export default function App() {
-  const [notes, setNotes] = React.useState(
-    JSON.parse(localStorage.getItem("notes")) || []
-  );
+  const [notes, setNotes] = React.useState([])
   const [currentNoteId, setCurrentNoteId] = React.useState(
+<<<<<<< HEAD
     (notes[0] && notes[0].id) || ""
   );
 
@@ -17,16 +16,22 @@ export default function App() {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
+=======
+      (notes[0] && notes[0].id) || ""
+  )
+  
+>>>>>>> parent of 7058002 (update App.jsx)
   function createNewNote() {
-    const newNote = {
-      id: nanoid(),
-      body: "# Type your markdown note's title here",
-    };
-    setNotes((prevNotes) => [newNote, ...prevNotes]);
-    setCurrentNoteId(newNote.id);
+      const newNote = {
+          id: nanoid(),
+          body: "# Type your markdown note's title here"
+      }
+      setNotes(prevNotes => [newNote, ...prevNotes])
+      setCurrentNoteId(newNote.id)
   }
-
+  
   function updateNote(text) {
+<<<<<<< HEAD
     setNotes((oldNotes) =>
       oldNotes.map((oldNote) => {
         return oldNote.id !== currentNoteId
@@ -34,17 +39,23 @@ export default function App() {
           : oldNote;
       })
     );
+=======
+      setNotes(oldNotes => oldNotes.map(oldNote => {
+          return oldNote.id === currentNoteId
+              ? { ...oldNote, body: text }
+              : oldNote
+      }))
+>>>>>>> parent of 7058002 (update App.jsx)
   }
-
+  
   function findCurrentNote() {
-    return (
-      notes.find((note) => {
-        return note.id === currentNoteId;
+      return notes.find(note => {
+          return note.id === currentNoteId
       }) || notes[0]
-    );
   }
-
+  
   return (
+<<<<<<< HEAD
     <main>
       {
       notes.length > 0 ? 
@@ -71,3 +82,44 @@ export default function App() {
     </main>
   );
 }
+=======
+      <main>
+      {
+          notes.length > 0 
+          ?
+          <Split 
+              sizes={[30, 70]} 
+              direction="horizontal" 
+              className="split"
+          >
+              <Sidebar
+                  notes={notes}
+                  currentNote={findCurrentNote()}
+                  setCurrentNoteId={setCurrentNoteId}
+                  newNote={createNewNote}
+              />
+              {
+                  currentNoteId && 
+                  notes.length > 0 &&
+                  <Editor 
+                      currentNote={findCurrentNote()} 
+                      updateNote={updateNote} 
+                  />
+              }
+          </Split>
+          :
+          <div className="no-notes">
+              <h1>You have no notes</h1>
+              <button 
+                  className="first-note" 
+                  onClick={createNewNote}
+              >
+                  Create one now
+              </button>
+          </div>
+          
+      }
+      </main>
+  )
+}
+>>>>>>> parent of 7058002 (update App.jsx)
