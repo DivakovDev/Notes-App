6070761940ea87a6068ac7a0e87c904d95bba9af
Liusy Divakov <divakovwork@gmail.com>
2023-04-12 14:11:10 +0300

    Revert Back to local storage creation because bug

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -8,19 +8,10 @@ import {nanoid} from "nanoid"
 export default function App() {
   const [notes, setNotes] = React.useState([])
   const [currentNoteId, setCurrentNoteId] = React.useState(
-<<<<<<< HEAD
-    (notes[0] && notes[0].id) || ""
-  );
 
-  React.useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
-
-=======
       (notes[0] && notes[0].id) || ""
   )
   
->>>>>>> parent of 7058002 (update App.jsx)
   function createNewNote() {
       const newNote = {
           id: nanoid(),
@@ -31,21 +22,11 @@ export default function App() {
   }
   
   function updateNote(text) {
-<<<<<<< HEAD
-    setNotes((oldNotes) =>
-      oldNotes.map((oldNote) => {
-        return oldNote.id !== currentNoteId
-          ? { ...oldNote, body: text }
-          : oldNote;
-      })
-    );
-=======
       setNotes(oldNotes => oldNotes.map(oldNote => {
           return oldNote.id === currentNoteId
               ? { ...oldNote, body: text }
               : oldNote
       }))
->>>>>>> parent of 7058002 (update App.jsx)
   }
   
   function findCurrentNote() {
@@ -55,34 +36,7 @@ export default function App() {
   }
   
   return (
-<<<<<<< HEAD
-    <main>
-      {
-      notes.length > 0 ? 
-      (
-        <Split sizes={[30, 70]} direction="horizontal" className="split">
-          <Sidebar
-            notes={notes}
-            currentNote={findCurrentNote()}
-            setCurrentNoteId={setCurrentNoteId}
-            newNote={createNewNote}
-          />
-          {currentNoteId && notes.length > 0 && (
-            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
-          )}
-        </Split>
-      ) : (
-        <div className="no-notes">
-          <h1>You have no notes</h1>
-          <button className="first-note" onClick={createNewNote}>
-            Create one now
-          </button>
-        </div>
-      )}
-    </main>
-  );
-}
-=======
+
       <main>
       {
           notes.length > 0 
@@ -122,4 +76,3 @@ export default function App() {
       </main>
   )
 }
->>>>>>> parent of 7058002 (update App.jsx)
